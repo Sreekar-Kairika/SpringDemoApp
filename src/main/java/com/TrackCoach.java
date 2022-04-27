2460c1aa0a036e05c96f9a9669ad9f386a791fcf
Sreekar-Kairika <sreekar.kairika@zemosolabs.com>
2022-04-27 09:32:48 +0530

    dependecny inversion

---
+++ src/main/java/com/TrackCoach.java
@@ -0,0 +1,20 @@
+package com;
+
+public class TrackCoach implements Coach {
+
+    private FortuneService fortuneService;
+    public TrackCoach(FortuneService fortuneService){
+        this.fortuneService = fortuneService;
+    }
+    public TrackCoach(){
+
+    }
+    public String getDailyWorkout() {
+        return "Run a hard 5k";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return " Just do it "+ fortuneService.getFortune();
+    }
+}
